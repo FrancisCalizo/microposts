85d1866aabba6578c492dd7e4183822b91be87e4
Francis Calizo <francisacalizo@gmail.com>
2019-08-03 08:02:16 -0400

    Add Create Post functionality

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 import { http } from './http.js';
 import { ui } from './ui.js';
 
-http
-  .get('http://localhost:3000/Posts')
-  .then(data => {
-    ui.showPosts(data);
-  })
-  .catch(err => console.log(err));
+// Load Posts on Page Load
+window.addEventListener('DOMContentLoaded', e => {
+  http
+    .get('http://localhost:3000/Posts')
+    .then(data => {
+      ui.showPosts(data);
+    })
+    .catch(err => console.log(err));
+});
+
+// Create New Post
+document.querySelector('.post-submit').addEventListener('click', e => {
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+
+  const data = { title: title, body: body };
+
+  http
+    .post('http://localhost:3000/posts', data)
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
+});
